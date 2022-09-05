baf305fbe5b04499cc3416c56b71fbbb8da5a43c
skanes17 <Scottskanes@gmail.com>
2022-09-05 21:10:07 -0230

    Turned PLAY button into PLAY/STOP toggle with Leon's guidance. Removed second button.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function bpmFunction() {    // function converts bpm to ms interval
+/* function bpmFunction() {    // function converts bpm to ms interval
     clearInterval(beatInterval); // should stop any existing beat - but doesn't?
 
     let bpm = document.getElementById("tempo").value;
     let bpmMessage = 60 / bpm * 1000;
     
-    document.getElementById("currentTempo").innerHTML = "Your current tempo is: " + bpm + " BPM.";
+    document.getElementById("currentTempo").innerHTML = `Your current tempo is: ${bpm} BPM.`;
     
     document.getElementById("beats").innerHTML = ""; // clears the paragraph
     document.getElementById("beats").innerHTML += "Beat "; // ensures the beat starts immediately on click
@@ -14,10 +14,9 @@ function bpmFunction() {    // function converts bpm to ms interval
     function displayMessage() { // adds "Beat"s to the paragraph with id="beats"
         document.getElementById("beats").innerHTML += "Beat ";
         
-        /* experimenting with adding audio
-        let bassDrum = document.getElementById("bassDrum");
-        bassDrum.play();
-        */
+        // experimenting with adding audio
+        // let bassDrum = document.getElementById("bassDrum");
+        // bassDrum.play();
     }
     var stopBut = document.getElementById("stopBut");
     stopBut.addEventListener("click", Stop);
@@ -25,5 +24,27 @@ function bpmFunction() {    // function converts bpm to ms interval
     function Stop() {
         clearInterval(beatInterval);
         document.getElementById("beats").innerHTML = ""; // clears the paragraph
+    }
+}  */
+
+let toggle;
+let myInterval;
+
+function bpmFunction() {    // function converts bpm to ms interval
+    if (toggle === false) { // stop button
+        document.getElementById("currentTempo").innerHTML = `You stopped the beat!`;
+        clearInterval(myInterval); // should stop any existing beat - but doesn't?
+        document.getElementById("beats").innerHTML = ""; // clears the paragraph
+        toggle = true;
+    } else {    // main beat production
+        let bpm = document.getElementById("tempo").value;
+        let bpmMessage = (60 / bpm) * 1000;
+        document.getElementById("currentTempo").innerHTML = `Your current tempo is: ${bpm} BPM.`;
+        document.getElementById("beats").innerHTML += "Beat "; // ensures the beat starts immediately on click
+        myInterval = setInterval(displayMessage,bpmMessage); // produces beats by running the below function at each interval
+        function displayMessage() { // adds "Beat"s to the paragraph with id="beats"
+            document.getElementById("beats").innerHTML += "Beat ";
+        }
+        toggle = false;
     }
 }
