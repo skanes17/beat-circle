88500424ba44a1ab593773bb78920d27cf671d6d
skanes17 <Scottskanes@gmail.com>
2022-09-06 11:54:00 -0230

    Added animated circle, could be made to pulse on each beat

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-/* function bpmFunction() {    // function converts bpm to ms interval
-    clearInterval(beatInterval); // should stop any existing beat - but doesn't?
-
-    let bpm = document.getElementById("tempo").value;
-    let bpmMessage = 60 / bpm * 1000;
-    
-    document.getElementById("currentTempo").innerHTML = `Your current tempo is: ${bpm} BPM.`;
-    
-    document.getElementById("beats").innerHTML = ""; // clears the paragraph
-    document.getElementById("beats").innerHTML += "Beat "; // ensures the beat starts immediately on click
-    
-    var beatInterval = setInterval(displayMessage,bpmMessage); // produces beats by running the below function at each interval
-
-    function displayMessage() { // adds "Beat"s to the paragraph with id="beats"
-        document.getElementById("beats").innerHTML += "Beat ";
-        
-        // experimenting with adding audio
-        // let bassDrum = document.getElementById("bassDrum");
-        // bassDrum.play();
-    }
-    var stopBut = document.getElementById("stopBut");
-    stopBut.addEventListener("click", Stop);
-
-    function Stop() {
-        clearInterval(beatInterval);
-        document.getElementById("beats").innerHTML = ""; // clears the paragraph
-    }
-}  */
-
 let toggle;
 let myInterval;
 
