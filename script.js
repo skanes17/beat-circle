51e44cd15c3cf9a0a4da9e6a7e8de60f605c5f36
skanes17 <Scottskanes@gmail.com>
2022-09-07 11:19:14 -0230

    sound added to clicks

--- script.js
+++ script.js
@@ -11,58 +11,40 @@ function bpmFunction() {    // function converts bpm to ms interval
     } else {    // main beat production
         selectElement = document.querySelector('#botNumber');
         output3 = selectElement.value;        
-        console.log(output3);
+
+        let bpm = document.querySelector("#tempo").value;        
+        let bpmMessage = (60 / (bpm * (output3 / 4))) * 1000;   // changes beat feel based on bottom number
+        document.querySelector("#currentTempo").innerHTML = `Your current tempo is: ${bpm} BPM.`;
+        document.querySelector("#beats").innerHTML += "BEAT "; // ensures the beat starts immediately on click
+
+        let woodblock = new Audio("woodblock.mp3");
+        woodblock.volume = 0.5;
+        woodblock.play();
+
+        myInterval = setInterval(displayMessage,bpmMessage); // produces beats by running the below function at each interval
         
-        let bpm = document.querySelector("#tempo").value;
-        
-        // To Do modify the if statement to only modify the bpmMessage calculation
-        // This is will allow the code to be written just once
-        if (output3 === "4") {  
-            console.log("Standard Time!")
-            let bpmMessage = (60 / bpm) * 1000;
-            document.querySelector("#currentTempo").innerHTML = `Your current tempo is: ${bpm} BPM.`;
-            document.querySelector("#beats").innerHTML += "BEAT "; // ensures the beat starts immediately on click
-            myInterval = setInterval(displayMessage,bpmMessage); // produces beats by running the below function at each interval
-            
-            let i = 1;
-            function displayMessage() { // adds strong and weak beats to the paragraph with id="beats"
-                selectElement = document.querySelector('#topNumber');
-                output2 = selectElement.value;
-        
-                if(i % output2 === 0) {
-                    document.querySelector("#beats").innerHTML += "BEAT ";
-                    i += 1;
-                } else {
-                    document.querySelector("#beats").innerHTML += "Beat ";
-                    i += 1;
-                }      
-            }
-            document.querySelector("#startStopBut").innerHTML = "Stop the Beat!"; 
-            toggle = false;
-        } else {
-            console.log("Double Time!")
-            let bpmMessage = (60 / (bpm * 2)) * 1000;
-            document.querySelector("#currentTempo").innerHTML = `Your current tempo is: ${bpm} BPM.`;
-            document.querySelector("#beats").innerHTML += "BEAT "; // ensures the beat starts immediately on click
-            myInterval = setInterval(displayMessage,bpmMessage); // produces beats by running the below function at each interval
-            
-            let i = 1;
-            function displayMessage() { // adds strong and weak beats to the paragraph with id="beats"
-                selectElement = document.querySelector('#topNumber');
-                output2 = selectElement.value;
-        
-                if(i % output2 === 0) {
-                    document.querySelector("#beats").innerHTML += "BEAT ";
-                    i += 1;
-                } else {
-                    document.querySelector("#beats").innerHTML += "Beat ";
-                    i += 1;
-                }      
-            }
-            document.querySelector("#startStopBut").innerHTML = "Stop the Beat!"; 
-            toggle = false;
-        }        
-    }
+        let i = 1;
+        function displayMessage() { // adds strong and weak beats to the paragraph with id="beats"
+            selectElement = document.querySelector('#topNumber');
+            output2 = selectElement.value;
+    
+            if(i % output2 === 0) {
+                document.querySelector("#beats").innerHTML += "BEAT ";
+                let woodblock = new Audio("woodblock.mp3");
+                woodblock.volume = 0.5;
+                woodblock.play();
+                i += 1;
+            } else {
+                document.querySelector("#beats").innerHTML += "Beat ";
+                let korgClick = new Audio("korgClick.mp3");
+                korgClick.volume = 0.2;
+                korgClick.play();
+                i += 1;
+            }      
+        }
+        document.querySelector("#startStopBut").innerHTML = "Stop the Beat!"; 
+        toggle = false;
+    }        
 }
 
 // I found a similar code on w3schools
