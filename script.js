4e01e183ffeed252d424cbe3520e49cde51b8f7b
skanes17 <Scottskanes@gmail.com>
2022-09-07 18:43:17 -0230

    added stylesheet and image of music room

--- script.js
+++ script.js
@@ -8,63 +8,56 @@ function bpmFunction() {    // function converts bpm to ms interval
         document.querySelector("#beats").innerHTML = ""; // clears the paragraph
         document.querySelector("#startStopBut").innerHTML = "Start the Beat!"; 
         toggle = true;
-    } else {    // main beat production
-        selectElement = document.querySelector('#botNumber');
-        output3 = selectElement.value;        
-
-        let bpm = document.querySelector("#tempo").value;        
-        let bpmMessage = (60 / (bpm * (output3 / 4))) * 1000;   // changes beat feel based on bottom number
-        document.querySelector("#currentTempo").innerHTML = `Your current tempo is: ${bpm} BPM.`;
-        document.querySelector("#beats").innerHTML += "BEAT "; // ensures the beat starts immediately on click
-
-        soundState = document.querySelector("#volumeToggle");
-        if (soundState.checked == true){
-            let woodblock = new Audio("woodblock.mp3");            
-            woodblock.volume = 0.5;
-            woodblock.play();
-        }
-
-        myInterval = setInterval(displayMessage,bpmMessage); // produces beats by running the below function at each interval
+        return;     // read up on return a little more; helped remove the else statement
+    } 
         
-        let i = 1;
-        function displayMessage() { // adds strong and weak beats to the paragraph with id="beats"
-            selectElement = document.querySelector('#topNumber');
-            output2 = selectElement.value;
+    // main beat production
+    let output3 = document.querySelector('#botNumber').value;
+
+    let bpm = document.querySelector("#tempo").value;        
+    let bpmMessage = (60 / (bpm * (output3 / 4))) * 1000;   // changes beat feel based on bottom number
+    document.querySelector("#currentTempo").innerHTML = `Your current tempo is: ${bpm} BPM.`;
+    document.querySelector("#beats").innerHTML += "BEAT "; // ensures the beat starts immediately on click
+
+    soundState = document.querySelector("#volumeToggle");
+    if (soundState.checked == true){
+        let woodblock = new Audio("woodblock.mp3");            
+        woodblock.volume = 0.5;
+        woodblock.play();
+    }
+
+    myInterval = setInterval(displayMessage,bpmMessage); // produces beats by running the below function at each interval
     
-            if(i % output2 === 0) {
-                document.querySelector("#beats").innerHTML += "BEAT ";
-                if (soundState.checked == true){
-                    let woodblock = new Audio("woodblock.mp3");
-                    woodblock.volume = 0.5;
-                    woodblock.play();
-                }
-                i += 1;
-            } else {
-                document.querySelector("#beats").innerHTML += "Beat ";
-                if (soundState.checked == true){
-                    let korgClick = new Audio("korgClick.mp3");
-                    korgClick.volume = 0.2;
-                    korgClick.play();
-                }
-                i += 1;
-            }      
-        }
-        document.querySelector("#startStopBut").innerHTML = "Stop the Beat!"; 
-        toggle = false;
-    }        
-}
-
-// I found a similar code on w3schools
-// need more practice to pick it apart and build it from scratch
-// plan to replace outputs with beat manipulation
-function checkInputs() {
-        selectElement = document.querySelector('#tempo');
-        output1 = selectElement.value;
-        
+    let i = 1;
+    function displayMessage() { // adds strong and weak beats to the paragraph with id="beats"
         selectElement = document.querySelector('#topNumber');
         output2 = selectElement.value;
-    
-        selectElement = document.querySelector('#botNumber');
-        output3 = selectElement.value;
+
+        if(i % output2 === 0) {
+            document.querySelector("#beats").innerHTML += "BEAT ";
+            if (soundState.checked == true){
+                let woodblock = new Audio("woodblock.mp3");
+                woodblock.volume = 0.5;
+                woodblock.play();
+            }
+            i += 1;
+        } else {
+            document.querySelector("#beats").innerHTML += "Beat ";
+            if (soundState.checked == true){
+                let korgClick = new Audio("korgClick.mp3");
+                korgClick.volume = 0.2;
+                korgClick.play();
+            }
+            i += 1;
+        }      
+    }
+    document.querySelector("#startStopBut").innerHTML = "Stop the Beat!"; 
+    toggle = false;      
+}
+
+function checkInputs() {
+        let output1 = document.querySelector('#tempo').value;       
+        let output2 = document.querySelector('#topNumber').value;
+        let output3 =  document.querySelector('#botNumber').value;
         document.querySelector('.tempoTimeSigOutput').textContent = `You've chosen a tempo of ${output1} bpm with a time signature of ${output2}/${output3}.`;
 }
